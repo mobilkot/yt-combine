bb4e54ada0f50bcd4acfc6ba02e436df7396d9c2
mobilkot <mobilkot@yandex.com>
2018-09-17 17:50:13 +0400

    Update and rename logic_081.js to logic_082.js

--- release/logic_082.js
+++ release/logic_082.js
@@ -920,7 +920,7 @@ function summaryOutput2(type, apps, appt, cminut, cgbites) {
 
 
     if (cgbites === "0") { text += phrasesInet[4].phrase; }
-    else if (cgbites <= 7) {  text += phrasesInet[0].phrase;  }
+    else if (cgbites <= 3) {  text += phrasesInet[0].phrase;  }
     else if (cgbites <= 15) { text += phrasesInet[1].phrase; }
     else if (cgbites <= 30) { text += phrasesInet[2].phrase; }
     else if (cgbites > 30) { text += phrasesInet[3].phrase; }
